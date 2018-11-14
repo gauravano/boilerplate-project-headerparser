df09642f9b84f8dbcb219b0a87e3ab6c2402c528
gauravano <sachdeva.gaurav1997@gmail.com>
2018-11-14 23:34:05 +0530

    api made

--- server.js
+++ server.js
@@ -15,10 +15,14 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
+});
+
+app.get('/api/whoami', (req, res) => {
+    console.log(req.headers);
+    res.json({"ipaddress": req.connection.remoteAddress, "language": req.headers['accept-language'], "software" : req.headers['user-agent'] });
 });
 
 // listen for requests :)
